b40474b80c06ae02401d3d917b3760c64a72624c
Jacob Hansen <jhansen5861@gmail.com>
2022-07-12 10:27:08 -0600

    commit heartbeat

--- buildroot/package/heartbeat/src/heartbeat.c
+++ buildroot/package/heartbeat/src/heartbeat.c
@@ -2,15 +2,27 @@
 #include <unistd.h>
 #include <time.h>
 
-int main() {
+
+#define DEFAULT_TIME 	2
+
+
+int main(int argc, char **argv) {
+    int SLEEP_LENGTH = -1;
     time_t t = time(NULL);
     struct tm tm;
     
+    if (argc > 1) {
+    	SLEEP_LENGTH = strtol(argv[0]);
+    }
+    if (SLEEP_LENGTH < 1) {
+    	SLEEP_LENGTH = DEFAULT_TIME;
+    }
+           
     while(1) {
         t = time(NULL);
         tm = *localtime(&t);
         printf("[Heartbeat] - Still Breathing at time: %d:%d:%d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
-        sleep(5);
+        sleep(SLEEP_LENGTH);
     }
     return 0;
 }

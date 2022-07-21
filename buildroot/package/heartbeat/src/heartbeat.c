a776028ec10954377f3ac8721f46016c28d89e3f
Jacob Hansen <jhansen5861@gmail.com>
2022-07-21 15:18:21 -0600

    Updated Buildroot to latest version

--- buildroot/package/heartbeat/src/heartbeat.c
+++ buildroot/package/heartbeat/src/heartbeat.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
 
-
-#define DEFAULT_TIME 	2
-
-
-int main(int argc, char **argv) {
-    int SLEEP_LENGTH = -1;
+int main(int argc, char *argv[]) {
+    short SLEEP_LENGTH = -1;
     time_t t = time(NULL);
     struct tm tm;
-    
-    if (argc > 1) {
-    	SLEEP_LENGTH = strtol(argv[0]);
-    }
+    char ch;   
+     
+    FILE *fs;
+    fs = fopen("/root/sleeplength.txt", "r");
+    ch = fgetc(fs); 
+    SLEEP_LENGTH = atoi(&ch);
+       
     if (SLEEP_LENGTH < 1) {
-    	SLEEP_LENGTH = DEFAULT_TIME;
+     	SLEEP_LENGTH = 2;
     }
            
-    while(1) {
+    //while(1) {
+    for(int i = 0; i < 4; i++) {
         t = time(NULL);
         tm = *localtime(&t);
         printf("[Heartbeat] - Still Breathing at time: %d:%d:%d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
